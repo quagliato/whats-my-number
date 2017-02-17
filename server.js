3f2b64eebaabb606a8348906b9a5e6c5107db80c
Eduardo Quagliato <eduardo@quagliato.me>
2017-02-17 15:06:01 -0200

    write or not the log file

--- server.js
+++ server.js
@@ -12,25 +12,28 @@ const moment                 = require('moment');
 const expressApp = new express();
 
 const EXPRESS_PORT = process.env.WHATS_MY_NUMBER_PORT || 80;
-const LOG_FILE = process.env.WHATS_MY_NUMBER_LOG || 'requests.log';
+const LOG_FILE = process.env.WHATS_MY_NUMBER_LOGFILE || 'requests.log';
+const WRITE_FILE = process.env.WHATS_MY_NUMBER_LOG || false;
 
 // Listens to every method
 expressApp.all('/', function(req, res){
   // Saves log
   const logLine = JSON.stringify({
     date: moment().format('YYYY-MM-DD HH:mm:ss.SSS Z'),
-    IP: req.ip
+    IP: req.headers['x-forwarded-for'] || req.connection.remoteAddress
   });
 
   console.log(logLine);
 
-  fs.appendFileSync(LOG_FILE, logLine + '\n', {
-    encoding: 'utf8'
-  });
+  if (WRITE_FILE !== false) {
+    fs.appendFileSync(LOG_FILE, logLine + '\n', {
+      encoding: 'utf8'
+    });
+  }
 
   // Answer the address
   res.json({
-    IP: req.ip
+    IP: req.headers['x-forwarded-for'] || req.connection.remoteAddress
   });
 });
 
